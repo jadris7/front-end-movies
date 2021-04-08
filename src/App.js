fcd6e2ed8d26d89496b0780bc3979303957a53c9
jadris7 <aguilaradry7@gmail.com>
2021-04-07 22:16:20 -0500

    Primera Versión del proyecto

---
+++ src/App.js
@@ -0,0 +1,18 @@
+//import logo from './logo.svg';
+import './App.css';
+import NavBar from './components/NavBar';
+import Table from './components/Table';
+
+function App() {
+
+  const texto = "Peliculas IUDigital";
+
+  return (
+    <div className="container">
+      <NavBar titulo={texto}/>
+      <Table />
+    </div>
+  );
+}
+
+export default App;
